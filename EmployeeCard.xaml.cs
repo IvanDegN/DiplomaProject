58c766ad600bccd3719f8a8b250464129114ed42
IvanDegN <mauzer.bruno@yandex.ru>
2022-05-15 18:30:19 +0300

    Добавьте файлы проекта.

---
+++ EmployeeCard.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DiplomaProject
+{
+    /// <summary>
+    /// Логика взаимодействия для EmployeeCard.xaml
+    /// </summary>
+    public partial class EmployeeCard : Window
+    {
+        public EmployeeCard()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnChangeImage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnShowStaffingTable_Click(object sender, RoutedEventArgs e)
+        {
+            StaffingTable staffingTable = new StaffingTable();
+            this.Close();
+            staffingTable.Show();
+        }
+
+        private void BtnPrintEmployeeCard_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+            StaffingTable staffingTable = new StaffingTable();
+            this.Close();
+            staffingTable.Show();
+        }
+    }
+}
---
+++ StaffingTable.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DiplomaProject
+{
+    /// <summary>
+    /// Логика взаимодействия для StaffingTable.xaml
+    /// </summary>
+    public partial class StaffingTable : Window
+    {
+        public StaffingTable()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGoBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnPrint_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
